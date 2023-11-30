b8b998678022c23ac64f144f8e8d16494a5775ae
sergey <SelickSD@Yandex.ru>
2023-11-30 21:04:22 +0300

    Свёрстан Launch Screen приложения. На главном экране — две вкладки: «Трекеры» и «Статистика». Вторая вкладка пока пустая. Свёрстан главный экран «Трекеры»: Заголовок — «Трекеры». в левом NavBarItem есть кнопка + для добавления новых трекеров; при нажатии на нее, пока что ничего не происходит - реализацию логики мы выполним в следующих уроках. если список трекеров для отображения пуст (в том числе — как результат поиска и/или выбора даты), то появляется заглушка;

---
+++ Tracker/Extensions/UIColor + Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor + Extensions.swift
+//  Tracker
+//
+//  Created by Сергей Денисенко on 30.11.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YPBlack") ?? UIColor.black }
+    static var ypBlue: UIColor { UIColor(named: "YPBlue") ?? UIColor.blue }
+    static var ypWhite: UIColor { UIColor(named: "YPWhite") ?? UIColor.white }
+}
---
+++ Tracker/Navigation/TabBarController.swift
@@ -0,0 +1,73 @@
+//
+//  TabBarController.swift
+//  Tracker
+//
+//  Created by Сергей Денисенко on 30.11.2023.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .ypWhite
+        setupTabBarController()
+    }
+
+    private func setupTabBarController() {
+
+        let items: [TabBarItem] = [.trackers, .statistic]
+
+        self.viewControllers = items.map({tabBarItem in
+            switch tabBarItem {
+            case .trackers:
+                return createNavigationController(for: TrackersViewController(), index: .trackers)
+            case .statistic:
+                return createNavigationController(for: StatisticViewController(), index: .statistic)
+            }
+        })
+    }
+
+    private func createNavigationController(for rootViewController: UIViewController,
+                                            index: TabBarItem) -> UIViewController {
+
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        rootViewController.navigationItem.title = index.label
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.tabBarItem.title = index.label
+        navigationController.tabBarItem.image = index.image
+        return navigationController
+    }
+
+}
+
+// MARK: - TabBarItem
+
+extension TabBarController {
+
+    private enum TabBarItem {
+        case trackers
+        case statistic
+
+        var label: String {
+            switch self {
+            case .trackers:
+                return "Трекеры"
+            case .statistic:
+                return "Статистика"
+            }
+        }
+
+        var image: UIImage? {
+            switch self {
+            case .trackers:
+                return UIImage(named: "Trackers")
+            case .statistic:
+                return UIImage(named: "Statistic")
+            }
+        }
+    }
+
+}
